a37c41e6c537fbe742c018e06f351a549f848632
Gilbert Subay <gilbertttsubay@gmail.com>
2022-04-29 11:47:56 +0700

    first push

---
+++ ios-sdk/ios_sdk.h
@@ -0,0 +1,18 @@
+//
+//  ios_sdk.h
+//  ios-sdk
+//
+//  Created by Gilbert on 4/28/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ios_sdk.
+FOUNDATION_EXPORT double ios_sdkVersionNumber;
+
+//! Project version string for ios_sdk.
+FOUNDATION_EXPORT const unsigned char ios_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ios_sdk/PublicHeader.h>
+
+
